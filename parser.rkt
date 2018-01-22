c10ce2cc92bf83825e0ac38125024ceabf98f1ae
Elle Sullivan <elle@elles-mbp.home>
2018-01-22 16:42:55 +0000

    MC: remove redundant comments

--- parser.rkt
+++ parser.rkt
@@ -21,7 +21,7 @@ a-sum : [a-sum ("+"|"-")] a-product
 a-product : [a-product ("*"|"/"|"mod")] a-neg
 a-neg : ["-"] a-expt
 a-expt : [a-expt "^"] a-value
-@a-value : a-number | a-variable-id | /"(" a-expr /")" ;TODO: as written, this could allow bones (or other things associated with identifiers)
+@a-value : a-number | a-variable-id | /"(" a-expr /")"
 a-number : INTEGER | DECIMAL
 
 ;allowed point expressions: sum, subtract, unary negation, scaler multiplication
@@ -30,7 +30,7 @@ a-point-expr: a-point-sum
 a-point-sum: [a-point-sum ("+"|"-")] a-point-product
 a-point-product : a-expr * a-point-neg | a-point-neg * a-expr
 a-point-neg : ["-"] a-point-value
-@a-point-value : a-point | a-point-id | /"(" a-point-expr /")" ;TODO: numbers and stuff could sneak in as a-id
+@a-point-value : a-point | a-point-id | /"(" a-point-expr /")"
 a-point : /"[" a-expr /"," a-expr /"]"
 
 ;todo transformations, variables, trapesium, parameters, comments, duplicates?, sections (with scaling)
