ae4e6b13339f403aa94e4af1021544307011a063
Elle Sullivan <ellesailenesullivan@gmail.com>
2019-01-02 00:22:23 +0000

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,16 @@
+#lang info
+(define collection "Anatomy")
+(define version "1.0")
+(define test-omit-paths '("lexer-test.rkt" "parse-test.rkt"))
+(define deps '("base"
+               "beautiful-racket-lib"
+               "br-parser-tools-lib"
+               "brag"
+               "draw-lib"
+               "gui-lib"
+               "rackunit-lib"
+               "syntax-color-lib"
+               "web-server"
+               "json"
+               "list"))
+(define build-deps '("racket-doc"))
