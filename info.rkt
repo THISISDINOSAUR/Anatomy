2d79de35e4f264956711deb755ce54620b912501
Elle Sullivan <ellesailenesullivan@gmail.com>
2019-01-02 00:41:46 +0000

    Update dependancies in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,16 +1,10 @@
 #lang info
 (define collection "anatomy")
 (define version "1.0")
-(define test-omit-paths '("lexer-test.rkt" "parse-test.rkt"))
 (define deps '("base"
                "beautiful-racket-lib"
                "br-parser-tools-lib"
                "brag"
-               "draw-lib"
-               "gui-lib"
                "rackunit-lib"
-               "syntax-color-lib"
-               "web-server"
-               "json"
-               "list"))
+               "web-server-lib"))
 (define build-deps '("racket-doc"))
