667fa4bd3502b4a6b1f6dc62b9d93e6f37c226ce
Elle Sullivan <ellesailenesullivan@gmail.com>
2020-03-28 19:49:14 +0000

    Add testing structure to make testing polygon trees easier

--- structs/polygon-tree-test.rkt
+++ structs/polygon-tree-test.rkt
@@ -13,6 +13,7 @@
 
 (define (test-polygon) (test-polygon-with-scale 1 1 1))
 
+(struct test-structure (node1 node2 node3 node4 node5))
 #|
 1. ____
   |____|   __
@@ -52,40 +53,62 @@
                            (scale-point-dimension-wise (point 150 50 0) x y z)
                            (scale-point-dimension-wise (point 50 50 0) x y z)
                            -90)
-  root)
+  (test-structure root node2 node3 node4 node5))
 
 (define (test-tree) (test-tree-with-scale 1 1 1))
 
 
-
+; Test polygon-tree->polygons
 (define test-polygons (list (test-polygon) (test-polygon) (test-polygon) (test-polygon) (test-polygon)))
-(check-equal? (polygon-tree->polygons (test-tree)) test-polygons)
+(check-equal? (polygon-tree->polygons (test-structure-node1 (test-tree))) test-polygons)
 
+; Test scale-polygon-tree!
 (define test-tree-scaled (test-tree))
-(scale-polygon-tree! test-tree-scaled 2 2 1)
-(for ([(polygon) (polygon-tree->polygons test-tree-scaled)])
+(define node1 (test-structure-node1 test-tree-scaled))
+(scale-polygon-tree! node1 2 2 1)
+(for ([(polygon) (polygon-tree->polygons node1)])
   (check-equal? polygon (test-polygon-with-scale 2 2 1)))
 
-;test connection points
-(define node2 (car (polygon-tree-children test-tree-scaled)))
+(define node2 (test-structure-node2 test-tree-scaled))
 (check-equal? (polygon-tree-connection-point-on-parent node2) (point 300 100 0))
 (check-equal? (polygon-tree-connection-point node2) (point 100 100 0))
-(define node5 (car (cdr (polygon-tree-children (car (polygon-tree-children node2))))))
+(define node5 (test-structure-node5 test-tree-scaled))
 (check-equal? (polygon-tree-connection-point-on-parent node5) (point 300 100 0))
 (check-equal? (polygon-tree-connection-point node5) (point 100 100 0))
 
 
 (define test-tree-scaled-from-node2 (test-tree))
-(scale-polygon-tree! (car (polygon-tree-children test-tree-scaled-from-node2)) 1.5 1 1)
-(for ([(polygon) (polygon-tree->polygons (car (polygon-tree-children test-tree-scaled-from-node2)))])
+(set! node2 (test-structure-node2 test-tree-scaled-from-node2))
+(scale-polygon-tree! node2 1.5 1 1)
+(for ([(polygon) (polygon-tree->polygons node2)])
   (check-equal? polygon (test-polygon-with-scale 1.5 1 1)))
 ;root node shouldn't scale
-(check-equal? (polygon-tree-polygon test-tree-scaled-from-node2) (test-polygon))
+(check-equal? (polygon-tree-polygon (test-structure-node1 test-tree-scaled-from-node2)) (test-polygon))
 
-(set! node2 (car (polygon-tree-children test-tree-scaled-from-node2)))
 ;connection point on parent (root) shouldn't scale, but everything else should
 (check-equal? (polygon-tree-connection-point-on-parent node2) (point 150 50 0))
 (check-equal? (polygon-tree-connection-point node2) (point 75.0 50 0))
-(set! node5 (car (cdr (polygon-tree-children (car (polygon-tree-children node2))))))
+(set! node5 (test-structure-node5 test-tree-scaled-from-node2))
 (check-equal? (polygon-tree-connection-point-on-parent node5) (point 225.0 50 0))
 (check-equal? (polygon-tree-connection-point node5) (point 75.0 50 0))
+
+
+; Test scale-root-only-of-polygon-tree!
+(define test-tree-node2-scaled (test-tree))
+(set! node1 (test-structure-node1 test-tree-node2-scaled))
+(set! node2 (test-structure-node2 test-tree-node2-scaled))
+(scale-root-only-of-polygon-tree! node2 0.1 0.1 1)
+(check-equal? (polygon-tree-polygon node1) (test-polygon))
+(check-equal? (polygon-tree-polygon node2) (test-polygon-with-scale 0.1 0.1 1))
+(check-equal? (polygon-tree-connection-point-on-parent node2) (point 150 50 0))
+(check-equal? (polygon-tree-connection-point node2) (point 5.0 5.0 0))
+
+(define node3 (test-structure-node3 test-tree-node2-scaled))
+(check-equal? (polygon-tree-polygon node3) (test-polygon))
+(check-equal? (polygon-tree-connection-point-on-parent node3) (point 20.0 5.0 0))
+(check-equal? (polygon-tree-connection-point node3) (point 0 50 0))
+
+(define node4 (test-structure-node4 test-tree-node2-scaled))
+(check-equal? (polygon-tree-polygon node4) (test-polygon))
+(check-equal? (polygon-tree-connection-point-on-parent node4) (point 150 50 0))
+(check-equal? (polygon-tree-connection-point node4) (point 50 50 0))
