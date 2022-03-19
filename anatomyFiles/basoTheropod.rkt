79fd86d7ed5f62bff2e2d44dca9332ae972cbdd0
Elle Sullivan <ellesailenesullivan@gmail.com>
2022-03-19 02:04:15 +0000

    Update basotheropod

--- anatomyFiles/basoTheropod.rkt
+++ anatomyFiles/basoTheropod.rkt
@@ -87,17 +87,9 @@ illium ~ sacrum = [-21, 88] ~ [0, 0], 0
 dorsalSpine = [-8, 49], [100, 56], [233, 54], [331, 41], [443, 15], [523, 0], [583, 12], [650, 54], [716, 89], [803, 119], [863, 118], [856, 85], [807, 63], [740, 37], [693, 11], [648, -23], [594, -43], [520, -52], [423, -45], [328, -43], [218, -40], [133, -40], [48, -47], [7, -54]
 sacrum ~ dorsalSpine = [82, 8] ~ [0, 0], 0
 
-dorsalRibs = [-17, -28], [-137, -10], [-278, -6], [-536, -63], [-515, -193], [-411, -331], [-275, -375], [-42, -304], [0, -173]
-//dorsalSpine~dorsalRibs = [-276, 13] ~ 2, 0
+ribs = [-21, 15], [111, -6], [231, -20], [318, -18], [300, -45], [223, -86], [114, -197], [35, -200], [-46, -182], [-165, -110], [-228, -69], [-251, -49], [-238, 17], [-123, 25]
+dorsalSpine ~ ribs = [278, 7] ~ [0, 0], 0
 
-scapula = [-146.22, -38.765, 0], [-178.84, -23.12, 0], [-197.22, -23.12, 0], [-218.283, -55.085, 0], [-203.322, -91.81, 0], [-165.922, -91.12, 0], [-153.68, -82.28, 0], [-61.88, -80.92, 0], [-25.843, -90.441, 0], [-2.72, -117.641, 0], [12.922, -113.56, 0], [28.562, -87.04, 0], [17.0, -38.085, 0], [17.68, -24.48, 0], [-3.4004, -6.801, 0], [-30.6, -3.4004, 0], [-57.804, -21.76, 0], [-56.445, -36.04, 0], [-61.2, -43.52, 0]
-//dorsalSpine~scapula = [-194.5, -94] ~ average(1,4), 46.84
-
-coracoid = [-10.21, -1.36, 0], [-67.321, -3.44, 0], [-93.161, -36.04, 0], [-59.164, -60.52, 0], [-19.043, -51.1, 0], [-1.36, -12.24, 0]
-//scapula~coracoid = [23.8, -68.0, 0] ~ average(0,1), -90
-
-sternalPlate = [-2.72, -5.44, 0], [-73.441, -2.72, 0], [-95.2, -8.16, 0], [-128.52, -8.16, 0], [-102.68, -20.42, 0], [-48.28, -29.92, 0], [-6.81, -26.523, 0]
-coracoid~sternalPlate = 2 ~ last, 15
 //body end
 
 
@@ -108,14 +100,87 @@ dorsalSpine ~ scapula = [367, -9] ~ [0, 0], 0
 humerus = [11, 11], [11, 1], [4, -7], [4, -30], [-8, -55], [-22, -62], [-40, -57], [-62, -62], [-83, -76], [-92, -94], [-113, -98], [-123, -84], [-110, -73], [-58, -43], [-25, -23], [-14, -1], [-8, 15]
 scapula ~ humerus = [84, -162] ~ [0, 0], 0
 
+radius = [7, -2], [21, -18], [37, -37], [64, -58], [95, -89], [104, -100], [100, -106], [90, -106], [63, -74], [40, -56], [12, -28], [-2, -11], [-1, -2]
+humerus ~ radius = [-99, -94] ~ [0, 0], 0
+
+ulna = [-1, -1], [18, -18], [43, -47], [69, -70], [95, -98], [104, -110], [101, -117], [91, -119], [69, -90], [41, -63], [13, -36], [-12, -16], [-18, -1], [-16, 10], [-7, 8]
+humerus ~ ulna = [-118, -91] ~ [0, 0], 0
 
 
+// Hand
+// From a pracical stand point, for now I've just modeled the carpals as one bone.
+// Maybe we need bones that can have multiple parts, are bones that can be joined to form one unit?
+carpals = [4, -1], [13, 3], [22, -10], [8, -19], [-5, -22], [-19, -26], [-22, -16], [-13, -13], [-2, -10]
+radius ~ carpals = [96, -107] ~ [0, 0], 0
+
+anteriorMetacarpal = [8, 5], [11, -11], [17, -22], [24, -29], [23, -37], [13, -41], [7, -40], [4, -25], [-6, -14], [-7, -4], [0, -2]
+carpals ~ anteriorMetacarpal = [16, -14] ~ [0, 0], 0
+
+anteriorProximalPhalange = [7, 4], [18, -10], [28, -19], [29, -26], [19, -32], [12, -23], [4, -13], [-6, -9], [-6, -2], [0, 0]
+anteriorMetacarpal ~ anteriorProximalPhalange = [19, -40] ~ [0, 0], 0
+
+anteriorDistalPhalange = [5, 4], [18, -18], [26, -28], [30, -43], [12, -26], [-8, -6]
+anteriorProximalPhalange ~ anteriorDistalPhalange = [26, -29] ~ [0, 0], 0
+
+
+intermediateMetacarpal = [1, 0], [9, 1], [20, -24], [30, -47], [28, -55], [23, -59], [16, -62], [10, -35], [-1, -15], [-5, -10], [-5, -2]
+carpals ~ intermediateMetacarpal = [4, -22] ~ [0, 0], 0
+
+intermediateProximalPhalange = [10, -1], [10, -15], [16, -25], [21, -35], [8, -36], [5, -41], [1, -25], [-8, -6], [0, -5]
+intermediateMetacarpal ~ intermediateProximalPhalange = [23, -58] ~ [0, 0], 0
+
+intermediateIntermediatePhalange = [8, 2], [11, -11], [19, -23], [20, -32], [13, -34], [6, -38], [3, -26], [-3, -14], [-10, -2], [0, -1]
+intermediateProximalPhalange ~ intermediateIntermediatePhalange = [12, -36] ~ [0, 0], 0
+
+intermediateDistalPhalange = [7, 0], [14, -21], [25, -39], [35, -53], [13, -38], [-3, -15], [-8, -3], [-1, -3]
+intermediateIntermediatePhalange ~ intermediateDistalPhalange = [15, -34] ~ [0, 0], 0
+
+
+intermediate2Metacarpal = [10, -3], [15, -21], [16, -41], [27, -59], [32, -68], [20, -67], [9, -73], [8, -51], [5, -32], [-1, -18], [-3, -3]
+carpals ~ intermediate2Metacarpal = [-8, -24] ~ [0, 0], 0
+
+intermediate2ProximalPhalange = [10, -2], [9, -13], [15, -26], [17, -33], [7, -33], [-4, -33], [-3, -20], [-8, -7], [3, -5]
+intermediate2Metacarpal ~ intermediate2ProximalPhalange = [19, -67] ~ [0, 0], 0
+
+intermediate2intermediatePhalange = [10, -3], [10, -17], [13, -26], [1, -28], [-9, -3], [1, -4]
+intermediate2ProximalPhalange ~ intermediate2intermediatePhalange = [8, -33] ~ [0, 0], 0
+
+intermediate2Intermediate2Phalange = [7, -1], [8, -13], [14, -29], [8, -29], [1, -31], [-5, -11], [-7, 0], [0, -1]
+intermediate2intermediatePhalange ~ intermediate2Intermediate2Phalange = [7, -28] ~ [0, 0], 0
+
+intermediate2DistalPhalange = [8, -3], [8, -22], [15, -51], [0, -29], [-8, -11], [-7, -3], [0, -4]
+intermediate2Intermediate2Phalange ~ intermediate2DistalPhalange = [7, -30] ~ [0, 0], 0
+
+
+posteriorMetacarpal = [0, 0], [7, 0], [8, -18], [15, -44], [7, -47], [0, -26], [-5, -17], [-5, -1]
+carpals ~ posteriorMetacarpal = [-18, -26] ~ [0, 0], 0
+
+
+neckTemp = [26, 21], [44, 22], [43, -10], [21, -8]
+dorsalSpine ~ neckTemp = [833, 98] ~ [0, 0], 0
+
+cranium = [4, 15], [21, 32], [50, 27], [65, 20], [80, 19], [91, 18], [105, 8], [117, -1], [170, -25], [213, -45], [240, -53], [257, -73], [263, -93], [261, -110], [253, -124], [233, -113], [207, -110], [181, -104], [159, -99], [121, -85], [95, -74], [78, -68], [42, -65], [17, -61], [-14, -52], [-55, -48], [-55, -39], [-26, -39], [-13, -9], [-13, 12], [-4, 19]
+neckTemp ~ cranium = [34, 8] ~ [0, 0], 0
+
+mandible = [3, 0], [8, -7], [38, -22], [61, -32], [82, -54], [106, -86], [123, -115], [146, -151], [174, -177], [194, -193], [200, -205], [183, -210], [165, -200], [132, -168], [97, -136], [71, -121], [53, -104], [32, -77], [15, -53], [1, -28], [-11, -12], [-9, 4], [-1, 12], [5, 9]
+cranium ~ mandible = [-39, -45] ~ [0, 0], 0
+
+
+tail = [0, 9], [0, -51], [-42, -105], [-86, -133], [-260, -121], [-488, -98], [-670, -81], [-837, -67], [-988, -67], [-1158, -65], [-1328, -93], [-1458, -137], [-1558, -170], [-1575, -170], [-1530, -144], [-1414, -100], [-1282, -60], [-1130, -30], [-958, -19], [-819, -7], [-672, -5], [-472, 16], [-305, 28], [-149, 40], [-58, 53], [-16, 65]
+sacrum ~ tail = [-79, -10] ~ [0, 0], 0
+
+
+//anterior posterior
+//metacarpals
+//proximal phalanges, intermediate phalanges, distal phalanges
 
 print illium
 print dorsalSpine
 render illium
 
-
+// Gastralium not connected to other bones. not sure what to do with that
+// maybe do the points in the same way as soft tissue?
+// I suppose technically they should be positioned relative to the soft tissue
 
 
 // OLD
